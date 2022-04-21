ca433450e8703ac4113c3021a340da7b567fbca5
Andrian-prayoga <1718054@scholar.itn.ac.id>
2022-04-21 10:58:24 +0700

    add ApiStepDef and Api service

---
+++ src/test/java/api/service/Api.java
@@ -0,0 +1,9 @@
+package api.service;
+
+import net.serenitybdd.rest.SerenityRest;
+import org.json.JSONObject;
+
+public class Api {
+
+    private static final String API_BASEURL = "";
+}
---
+++ src/test/java/api/stepdef/ApiStepDef.java
@@ -0,0 +1,27 @@
+package api.stepdef;
+
+import api.service.Api;
+import io.cucumber.java.en.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+
+public class ApiStepDef {
+    Api api = new Api();
+
+    //Action -- Begin
+
+    //Action -- End
+
+    //Validation -- Begin
+    @Then("response status code should be {int}")
+    public void statusCodeValidation(int statusCode) {
+        restAssuredThat(response -> response.statusCode(statusCode));
+    }
+
+    @Then("response structure should match json schema {string}")
+    public void validateJsonSchema(String schema) {
+        String path = String.format("schema/%s", schema);
+        restAssuredThat(response -> response.assertThat().body(matchesJsonSchemaInClasspath(path)));
+    }
+    //Validation -- End
+}
